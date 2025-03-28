a3564cf4ffcf7bdb3ca35f498f481b42ea358f4b
mscopa <maurocopa20@gmail.com>
2025-03-28 14:49:12 -0300

    individual assignment 2 dynamic product list

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -28,3 +28,11 @@ export function getParam(param) {
   const product = urlParams.get(param);
   return product
 }
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  const htmlStrings = list.map(templateFn).join("");
+  parentElement.insertAdjacentHTML(position, htmlStrings);
+}
