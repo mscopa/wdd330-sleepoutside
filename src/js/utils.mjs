b36e3380e14859775d6d1617180beaf6ef2e8bf9
mscopa <maurocopa20@gmail.com>
2025-07-25 12:01:19 -0300

    week 02 team activity core

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -36,3 +36,27 @@ export function renderListWithTeplate(template, parentElement, list, position = 
   }
   parentElement.insertAdjacentHTML(position, htmlString.join(""));
 }
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if(callback) {
+    callback(data);
+  }
+}
+
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+
+  const headerElement = document.querySelector("#header");
+  const footerElement = document.querySelector("#footer");
+  
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
