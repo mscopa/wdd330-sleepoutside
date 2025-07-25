15dc465ba8eaeb41958a9ee370704199eb658170
mscopa <maurocopa20@gmail.com>
2025-07-25 17:27:23 -0300

    week 03 lint format and build individual activity

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,7 +2,7 @@ import { renderListWithTeplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
     return `<li class="product-card">
-        <a href="product_pages/?product=${product.Id}">
+        <a href="/product_pages/?product=${product.Id}">
             <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
             <h2 class="card__brand">${product.Brand.Name}</h2>
             <h3 class="card__name">${product.NameWithoutBrand}</h3>
